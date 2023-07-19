9eb19493185104e8d08e2d3a49dea9b443d495c7
misskatiemarple <misskatiemarple@gmail.com>
2023-07-19 10:52:35 +0900

    Styled Hero section

--- src/Hero.js
+++ src/Hero.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { useTypewriter, Cursor } from "react-simple-typewriter";
-
 import "./Hero.css";
 
 export default function Hero() {
@@ -11,23 +10,27 @@ export default function Hero() {
     deleteSpeed: 80,
   });
   return (
-    <div className="container">
-      <span className="hero-text">
+    <div className="row hero-grid">
+      <div className="col-6 katie-text">
         <h2>Hello, I'm Katie, </h2>
-        <h1 style={{ margin: "50px" }}>
+        <h1>
           I'm a <span style={{ fontWeight: "bold" }}> {text}</span>
-          <Cursor cursorColor="red" />
+          <Cursor cursorColor="pink" />
         </h1>
         <h4>based in Japan</h4>
         <button>Hello Button </button>
-      </span>
-      <span className="hero-photo">
-        <img
-          src="/images/katie.jpg"
-          alt="Image of Katie Marple"
-          className="katie img-fluid "
-        />
-      </span>
+      </div>
+
+      <div className="col-6">
+        <div className="hero-photo">
+          <img
+            src="/images/katie.jpg"
+            style={{ width: "280px" }}
+            alt="Image of Katie Marple"
+            className="katie-image img-fluid rounded-circle mt-3"
+          />
+        </div>
+      </div>
     </div>
   );
 }
