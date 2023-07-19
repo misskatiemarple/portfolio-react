847f29db0273fc52b6bfd0101e4303eec7c617c1
misskatiemarple <misskatiemarple@gmail.com>
2023-07-19 11:13:54 +0900

    Centered hero component

--- src/Hero.js
+++ src/Hero.js
@@ -10,25 +10,29 @@ export default function Hero() {
     deleteSpeed: 80,
   });
   return (
-    <div className="row hero-grid">
-      <div className="col-6 katie-text">
-        <h2>Hello, I'm Katie, </h2>
-        <h1>
-          I'm a <span style={{ fontWeight: "bold" }}> {text}</span>
-          <Cursor cursorColor="pink" />
-        </h1>
-        <h4>based in Japan</h4>
-        <button>Hello Button </button>
-      </div>
+    <div className="container hero-grid-container">
+      <div className="row mb-4">
+        <div className="col-7 katie-text">
+          <h2>Hello, I'm Katie, </h2>
+          <h1>
+            I'm a <span style={{ fontWeight: "bold" }}> {text}</span>
+            <Cursor cursorColor="pink" />
+          </h1>
+          <h4>based in Japan</h4>
+          <button className="mt-4 ms-4 btn btn-primary">
+            What is this button for Marple?{" "}
+          </button>
+        </div>
 
-      <div className="col-6">
-        <div className="hero-photo">
-          <img
-            src="/images/katie.jpg"
-            style={{ width: "280px" }}
-            alt="Image of Katie Marple"
-            className="katie-image img-fluid rounded-circle mt-3"
-          />
+        <div className="col-5">
+          <div className="hero-photo">
+            <img
+              src="/images/katie.jpg"
+              style={{ width: "280px" }}
+              alt="Image of Katie Marple"
+              className="katie-image img-fluid rounded-circle mt-3 "
+            />
+          </div>
         </div>
       </div>
     </div>
