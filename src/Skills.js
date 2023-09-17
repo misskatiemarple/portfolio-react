4dbc2883b1b836903d98f43d32dc5211ece4de81
misskatiemarple <misskatiemarple@gmail.com>
2023-09-17 20:17:21 +0900

    text update

--- src/Skills.js
+++ src/Skills.js
@@ -6,8 +6,9 @@ export default function Skills() {
     <div className="skills-container">
       <h2 className="subtitle-h2 skills-subtitle">Skills.</h2>
       <p>
-        These are the skills that I have acquired to date. I am currently
-        learning Node.js and Next.js. I also have an interest in Ai.
+        These are the skills that I honed on the She Codes training. I have
+        since been developing my own projects and pages. I'm now moving onto
+        studying Flutter and Dart.
       </p>
       <img
         src="/images/skills.png"
