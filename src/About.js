8dc70e05f389ce8e383a58c3a09d6dcf60c0c231
misskatiemarple <misskatiemarple@gmail.com>
2023-09-23 14:37:50 +0900

    Update text

--- src/About.js
+++ src/About.js
@@ -11,11 +11,25 @@ export default function About() {
             About.
           </h2>
           <p className="aboutme-text">
-            {" "}
-            I decided to transition into coding because I am passionate about
-            learning new things, technology, and innovation. With my broad range
-            of experience, spanning life in seven countries, I have insight into
-            user-focused development.
+            <p>
+              I have lived in seven countries and traveled to many more. From
+              collaborating with Presidents to sleeping on the streets, my
+              journey includes life in seven countries and many years exploring
+              different countries.{" "}
+            </p>
+            <p>
+              Want to know something that we all share?{" "}
+              <strong>You have a problem.</strong>
+            </p>
+            <p>
+              It doesn't matter if you have one or a million dollars in the
+              bank; humans have problems. I want to solve our clients' problems.{" "}
+            </p>
+            <p>
+              My fundamental belief is that we can create an inclusive and
+              efficient world with the things we make. So, let's create
+              something groundbreaking.
+            </p>
           </p>
         </div>
         <div className="col-md">
