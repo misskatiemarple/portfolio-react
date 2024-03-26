bb8077382b7b1cfd286ad004de3aafd8b8f2ea7c
misskatiemarple <misskatiemarple@gmail.com>
2024-03-26 14:37:10 +0900

    updated skills text

--- src/Skills.js
+++ src/Skills.js
@@ -6,9 +6,17 @@ export default function Skills() {
     <div className="skills-container">
       <h2 className="subtitle-h2 skills-subtitle">Skills.</h2>
       <p>
-        I honed my skills during the SheCodes certification program. My passion
-        for programming continues to grow and I am now focussing on Flutter and
-        Dart.
+        I started my programming journey with{" "}
+        <a href="https://www.shecodes.io/workshop-max" target="_blank">
+          She Codes Max
+        </a>
+        , I acquired essential coding skills in HTML, CSS, and JavaScript,
+        including advanced techniques in ES6 for building interactive
+        applications. I also learned how to create responsive designs adaptable
+        to various screen sizes and developed proficiency in building
+        interactive text generators with AI integration. These newfound skills
+        have broadened my capabilities in web development and equipped me to
+        tackle diverse projects in the field.
       </p>
       <img
         src="/images/skills.png"
